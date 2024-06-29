ba9e1ee99aeb2138315723ec3732fb2853000e8b
lowen967 <owendali27@gmail.com>
2024-06-29 06:00:31 +0000

    pythonanywhere

--- strongwrapped/strongwrapped_project/strongwrapped_project/settings.py
+++ strongwrapped/strongwrapped_project/strongwrapped_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1n8^rw)4-8gj0%0bh*!@o^=so7ad-4^m9c+m57@s*x4+_5%w))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['owenl27.pythonanywhere.com']
 
 
 # Application definition
